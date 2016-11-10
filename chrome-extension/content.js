ecc0304d1d3ddf072b48fb0f76aa333686398e9f
Stanley Idesis <stan.idesis@gmail.com>
2016-11-10 10:15:11 -0800

    [RANDOM] Material JS Runs
    It required a kick-start because it previously relied on the window to
    load before registering and upgrading all MDL elements.
    
    Since we're injecting the element onto the page, we had to do it
    manually from the content script.

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,30 +1,34 @@
 // // Listen to messages from the background
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.message === "display_popup_at_cursor") {
+    if (request.message === 'display_popup_at_cursor') {
       // This is where we need to present a little auto-complete search input
       var activeElement = document.activeElement;
-      console.log("Active Element: " + activeElement.tagName);
-      if (activeElement.tagName !== "INPUT" && activeElement.tagName !== "TEXTAREA") {
+      console.log('Active Element: ' + activeElement.tagName);
+      if (activeElement.tagName !== 'INPUT' && activeElement.tagName !== 'TEXTAREA') {
         // Active element is not an input area
         return;
       }
       var coordinates = getCaretCoordinates(activeElement, activeElement.selectionEnd);
-      console.log(coordinates.top);
-      console.log(coordinates.left);
       var cumulativeOffset = getCumulativeOffset(activeElement);
-      console.log(cumulativeOffset.left);
-      console.log(cumulativeOffset.top);
       var fontSize = $(activeElement).css('font-size');
       var lineHeight = Math.floor(parseInt(fontSize.replace('px','')) * 2.0);
       $.get(chrome.extension.getURL('/popup.html'), function(data) {
         $($.parseHTML(data)).appendTo('body');
-        var $emojiPopup = $("#emoji-autocomplete-popup");
-        $emojiPopup.css("left", (cumulativeOffset.left + coordinates.left));
-        $emojiPopup.css("top", cumulativeOffset.top + lineHeight);
-        $.getScript(chrome.extension.getURL('/material.min.js'), function() {
-          console.log("Running Material Min");
-        });
+        var $emojiPopup = $('#emoji-autocomplete-popup');
+        $emojiPopup.css('left', (cumulativeOffset.left + coordinates.left));
+        $emojiPopup.css('top', cumulativeOffset.top + lineHeight);
+        // Copied from Material.js
+        if ('classList' in document.createElement('div') &&
+            'querySelector' in document &&
+            'addEventListener' in window && Array.prototype.forEach) {
+          document.documentElement.classList.add('mdl-js');
+          componentHandler.upgradeAllRegistered();
+        } else {
+          componentHandler.upgradeElement = function() {};
+          componentHandler.register = function() {};
+        }
+        // End copy of Material.js
       });
     }
   }
